c7b84df3b1620fc22e9d3c5d0c1872694b141131
Akhilender Reddy Vallab <n01520335@humber.ca>
2022-02-22 21:38:03 -0500

    Add project files.

---
+++ project2/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace project2.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ project2/Controllers/jaController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace project2.Controllers
+{
+    public class jaController : ApiController
+    {
+        [Route("api/Ja/J1/{s}/{m}/{l}")]
+
+        public string GetJ1(int s, int m, int l)
+        {
+            int happyScore = (1 * s) + (2 * m) + (3 * l);
+
+
+            if (happyScore >= 10)
+            {
+                return "Barley is happy!";
+            }
+            else
+            {
+                return "Barley is sad :(";
+            }
+
+        }
+    }
+}
