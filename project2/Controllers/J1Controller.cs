82358254b0d8a4e1a455026d1b389d46149ff6e8
Akhilender Reddy Vallab <n01520335@humber.ca>
2022-02-23 22:09:53 -0500

    J1 question

---
+++ project2/Controllers/J1Controller.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace project2.Controllers
+{
+    
+    
+    public class J1Controller : ApiController
+    {
+        [Route("api/J1/Trt/{s}/{m}/{l}")]
+
+        public string GetTrt(int s, int m, int l)
+        {
+            int happyScore = (1 * s) + (2 * m) + (3 * l);
+
+
+            if (happyScore >= 10)
+            {
+                return "Barley is happy!";
+            }
+            else
+            {
+                return "Barley is sad :(";
+            }
+
+        }
+    }
+}
