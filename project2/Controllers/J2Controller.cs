4b384eb717988ae080a23a25cc9c4acbf34cc957
Akhilender Reddy Vallab <n01520335@humber.ca>
2022-02-23 22:02:49 -0500

    J2 question

--- project2/Controllers/J2Controller.cs
+++ project2/Controllers/J2Controller.cs
@@ -12,39 +12,19 @@ namespace project2.Controllers
         [Route("api/J2/dice/{n}/{m}")]
         public string Getdice(int n, int m)
         {
-            int[] nside = new int[n];
-            int[] mside = new int[m];
             int counter = 0;
-
-            for (int i = 1; i < nside.Length; i++)
+            for(int i = 1; i <= m; i++)
             {
-
-                nside[i] = i;
-
-                int nNum = nside[i];
-
-
-                for (int j = 1; j< mside.Length; j++)
+                for(int j = 1; j <= n; j++)
                 {
-                    mside[j] = j;
-
-                    int mNum = mside[j];
-
-                    int sum = nNum + mNum;
-
-                    while(sum == 10)
+                    if(i + j == 10)
                     {
                         counter++;
-
                     }
-
-
                 }
             }
 
-            return "The no of ways is :" + counter;
-
-
+            return "There are " + counter + " total ways to get the sum 10";
 
         }
     }
