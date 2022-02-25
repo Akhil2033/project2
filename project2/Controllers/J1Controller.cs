ea90fecc445cec4c999d38a1d3e5361c4bf82145
Akhil2033 <98551281+Akhil2033@users.noreply.github.com>
2022-02-24 23:37:35 -0500

    Update J1Controller.cs

--- project2/Controllers/J1Controller.cs
+++ project2/Controllers/J1Controller.cs
@@ -7,7 +7,10 @@ using System.Web.Http;
 
 namespace project2.Controllers
 {
-    
+    // The question demands 3 inputs, which equate to 3 kinds of treats
+    // create a variable(happyScore) to accomidate "treat" formula
+    // check whether the  happyScore is greater than and equal to 10 
+    // returning a string message accordingly
     
     public class J1Controller : ApiController
     {
